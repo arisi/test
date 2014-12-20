b8a7a6944f73bb8a4d35a5aea686749457047dd0
Ari Siitonen <jalopuuverstas@gmail.com>
2014-12-20 17:42:18 +0200

    fix

--- koe.c
+++ koe.c
@@ -1,7 +1,15 @@
+#include <stdio.h>
 
 #define nn 12
-int main()
+int main(int argc,char **argv)
 {
   int x=nn;
+  printf("moikkas %d %d\n",x,argc);
   return x;
+
+}
+
+void koe() {
+  printf("jesh");
+
 }
